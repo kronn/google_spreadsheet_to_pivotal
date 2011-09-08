c11936df744fc0be7d8c24705a327a8a460f1144
Matthias Viehweger <kronn@kronn.de>
2011-09-08 15:26:33 +0200

    minimal RackApp to bridge Spreadsheet-data to Pivotal

---
+++ spreadsheet_to_pivotal.rb
@@ -0,0 +1,27 @@
+require 'sinatra/base'
+require
+
+class SpreadsheetToPivotal < Sinatra::Base
+  $spreadsheet = Spreadsheet.new(
+    ENV["SPREADSHEET_USERNAME"],
+    ENV["SPREADSHEET_PASSWORD"],
+    ENV["SPREADSHEET_KEY"],
+    ENV["SPREADSHEET_MAPPING"]
+  )
+  $pivotal = Pivotal.new(
+    ENV["SPREADSHEET_REQUESTOR"]
+  )
+
+  use Rack::Auth::Basic do |username, password|
+    username == ENV["HTTP_BASIC_USERNAME"] && password == ENV["HTTP_BASIC_PASSWORD"]
+  end
+
+  get "/" do
+    stories = $spreadsheet.stories
+
+    content_type "text/xml", :charset => "utf-8"
+
+    $pivotal.stories_to_xml(stories)
+  end
+end
+
