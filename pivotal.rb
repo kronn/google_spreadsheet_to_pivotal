0b443dcf56b5568dadd5abbe472ff0836afd8332
Matthias Viehweger <kronn@kronn.de>
2011-11-01 11:28:40 +0100

    changed order of fields in xml

--- pivotal.rb
+++ pivotal.rb
@@ -13,18 +13,16 @@ class Pivotal
     xml = Builder::XmlMarkup.new(:target => buffer, :indent => 2)
 
     xml.instruct!
-
     xml.external_stories(:type => "array") do
       stories.each do |story|
         xml.external_story do
           xml.external_id  story["id"]
           xml.name         story["name"]
           xml.description  story["description"]
-          xml.estimate     story["estimate"]
-
           xml.requested_by requestor
-          xml.story_type   "feature"
           xml.created_at({ :type => "datetime" }, story["created_at"])
+          xml.story_type   "feature"
+          xml.estimate     story["estimate"]
         end
       end
     end
